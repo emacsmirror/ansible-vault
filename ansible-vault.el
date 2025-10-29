zellio/ansible-vault-mode

Use (setq-local var value) to support older Emacs (such as 26.1 on CentOS 8.5)
Emacs 27.1 permits (setq-local [VARIABLE VALUE]...), earlier Emacs do not.

ansible-vault.el
@@ -150,11 +150,10 @@ save hooks to treat ansible-vault file as encrypted on disk.")
            (cipher-algorithm (caddr header-tokens))
            (vault-id (cadddr header-tokens)))
       (when (string= "$ANSIBLE_VAULT" format-id)
-        (setq-local
-         ansible-vault--header-format-id format-id
-         ansible-vault--header-version version
-         ansuble-vault--header-cipher-algorithm cipher-algorithm
-         ansible-vault--header-vault-id vault-id)))))
+        (setq-local ansible-vault--header-format-id format-id)
+        (setq-local ansible-vault--header-version version)
+        (setq-local ansuble-vault--header-cipher-algorithm cipher-algorithm)
+        (setq-local ansible-vault--header-vault-id vault-id)))))
 
 ;;;###autoload
 (defun ansible-vault--is-encrypted-vault-file ()
@@ -273,9 +272,8 @@ PASSWORD-FILE path to the stored secret for provided VAULT-ID."
                                         (call-interactively 'ansible-vault--request-password))))
                 (car (push (cons vault-id password-file) ansible-vault-vault-id-alist)))))
          (password-file (or password-file (cdr vault-id-pair))))
-    (setq-local
-     ansible-vault--vault-id vault-id
-     ansible-vault--password-file password-file)
+    (setq-local ansible-vault--vault-id vault-id)
+    (setq-local ansible-vault--password-file password-file)
     vault-id-pair))
 
 (defun ansible-vault--flush-password-file ()
@@ -333,9 +331,8 @@ its password file."
            (ansible-config-path (ansible-vault--process-config-files)))
       (cond
        (vault-id-pair
-        (setq-local
-         ansible-vault--vault-id (car vault-id-pair)
-         ansible-vault--password-file (cdr vault-id-pair)))
+        (setq-local ansible-vault--vault-id (car vault-id-pair))
+        (setq-local ansible-vault--password-file (cdr vault-id-pair)))
        (ansible-vault--header-vault-id
         (ansible-vault--request-vault-id ansible-vault--header-vault-id))
        (t (let* ((password-file

emacsmirror/ansible-vault

ansible-vault.el
@@ -187,10 +187,9 @@ The header \"$ANSIBLE_VAULT;1.1;AES256\" will be parsed into:
            (cipher-algorithm (caddr header-tokens))
            (vault-id (cadddr header-tokens)))
       (when (string= "$ANSIBLE_VAULT" format-id)
-        (setq-local
-         ansible-vault--header-version version
-         ansible-vault--header-cipher-algorithm cipher-algorithm
-         ansible-vault--header-vault-id vault-id)))))
+        (setq-local ansible-vault--header-version version)
+        (setq-local ansible-vault--header-cipher-algorithm cipher-algorithm)
+        (setq-local ansible-vault--header-vault-id vault-id)))))
 
 ;;;###autoload
 (defun ansible-vault--is-encrypted-vault-file ()
@@ -310,9 +309,8 @@ PASSWORD-FILE path to the stored secret for provided VAULT-ID."
                                         (call-interactively 'ansible-vault--request-password))))
                 (car (push (cons vault-id password-file) ansible-vault-vault-id-alist)))))
          (password-file (or password-file (cdr vault-id-pair))))
-    (setq-local
-     ansible-vault--vault-id vault-id
-     ansible-vault--password-file password-file)
+    (setq-local ansible-vault--vault-id vault-id)
+    (setq-local ansible-vault--password-file password-file)
     vault-id-pair))
 
 ;; side-effect: it sets ansible-vault--password-file
@@ -328,9 +326,8 @@ its password file."
            (ansible-config-path (ansible-vault--process-config-files)))
       (cond
        (vault-id-pair
-        (setq-local
-         ansible-vault--vault-id (car vault-id-pair)
-         ansible-vault--password-file (cdr vault-id-pair)))
+        (setq-local ansible-vault--vault-id (car vault-id-pair))
+        (setq-local ansible-vault--password-file (cdr vault-id-pair)))
        (ansible-vault--header-vault-id
         (ansible-vault--request-vault-id ansible-vault--header-vault-id))
        (t (let* ((password-file